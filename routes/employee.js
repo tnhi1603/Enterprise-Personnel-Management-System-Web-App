9061098e18c330cd9e2fd10501f5faf6b391fc04
tnhi1603 <21521236@gm.uit.edu.vn>
2024-07-06 04:36:48 +0700

    update_e

--- routes/employee.js
+++ routes/employee.js
@@ -24,59 +24,139 @@ router.get('/', (req, res) => {
     });
 });
 
-// Get employee details by ID
 router.get('/:id', (req, res) => {
-    const employeeId = req.params.id;
-    const query = `
-      SELECT 
-          Staff.StaffID AS EmployeeID,
-          Staff.StaffName AS EmployeeName,
-          StaffInfo.Gender,
-          StaffInfo.CCCD,
-          StaffInfo.Position,
-          StaffInfo.Birthplace,
-          StaffInfo.Nationality,
-          StaffInfo.Nation,
-          StaffInfo.Languages,
-          StaffInfo.CurrentAddress,
-          StaffInfo.DateOfJoining,
-          Staff.Email,
-          Staff.PhoneNumber,
-          Staff.DayOfBirth,
-          Department.Department,
-          Project.ProjectName
-      FROM 
-          Staff
-      LEFT JOIN 
-          StaffInfo ON Staff.StaffID = StaffInfo.StaffID
-      LEFT JOIN 
-          Department ON Staff.DepartmentID = Department.DepartmentID
-      LEFT JOIN 
-          Project ON Staff.ProjectID = Project.ProjectID
-      WHERE 
-          Staff.StaffID = ?;
-    `;
-    db.query(query, [employeeId], (err, results) => {
-      if (err) {
-        console.error('Error executing query:', err);
-        return res.status(500).send(err);
-      }
-      res.json(results[0]);
-    });
-  });
+  const employeeId = req.params.id;
   
-  // Update employee details by ID
-  router.put('/update/:id', (req, res) => {
-    const employeeId = req.params.id;
-    const { StaffName, PhoneNumber, DayOfBirth, DepartmentID, ProjectID, ReminderID } = req.body;
-    const query = 'UPDATE Staff SET StaffName = ?, PhoneNumber = ?, DayOfBirth = ?, DepartmentID = ?, ProjectID = ?, ReminderID = ? WHERE StaffID = ?';
-    db.query(query, [StaffName, PhoneNumber, DayOfBirth, DepartmentID, ProjectID, ReminderID, employeeId], (err, results) => {
+  const query = `
+    SELECT 
+      Staff.StaffID AS EmployeeID,
+      Staff.StaffName AS EmployeeName,
+      StaffInfo.Gender,
+      StaffInfo.CCCD,
+      StaffInfo.Position,
+      StaffInfo.Birthplace,
+      StaffInfo.Nationality,
+      StaffInfo.Nation,
+      StaffInfo.Languages,
+      StaffInfo.CurrentAddress,
+      StaffInfo.DateOfJoining,
+      Staff.Email,
+      Staff.PhoneNumber,
+      Staff.DayOfBirth,
+      Department.Department,
+      Project.ProjectName,
+      Login.username,
+      Login.password
+    FROM Staff
+    LEFT JOIN StaffInfo ON Staff.StaffID = StaffInfo.StaffID
+    LEFT JOIN Department ON Staff.DepartmentID = Department.DepartmentID
+    LEFT JOIN Project ON Staff.ProjectID = Project.ProjectID
+    LEFT JOIN Login ON Staff.StaffID = Login.StaffID
+    WHERE Staff.StaffID = ?`;
+
+  db.query(query, [employeeId], (error, results) => {
+    if (error) {
+      res.status(500).send(error);
+    } else if (results.length === 0) {
+      res.status(404).send('Employee not found');
+    } else {
+      res.status(200).json(results[0]);
+    }
+  });
+});
+
+// Endpoint to update employee details
+router.put('/update/:id', (req, res) => {
+  const employeeId = req.params.id;
+  const {
+    StaffName,
+    PhoneNumber,
+    DayOfBirth,
+    DepartmentID,
+    ProjectID,
+    ReminderID,
+    Gender,
+    CCCD,
+    Position,
+    Birthplace,
+    Nationality,
+    Nation,
+    Languages,
+    CurrentAddress,
+    DateOfJoining,
+    Email,
+    username,
+    password
+  } = req.body;
+
+  const updateEmployeeQuery = `
+    UPDATE Staff 
+    SET StaffName = ?, PhoneNumber = ?, DayOfBirth = ?, DepartmentID = ?, ProjectID = ?, ReminderID = ?, Email = ?
+    WHERE StaffID = ?`;
+
+  const updateStaffInfoQuery = `
+    UPDATE StaffInfo 
+    SET Gender = ?, CCCD = ?, Position = ?, Birthplace = ?, Nationality = ?, Nation = ?, Languages = ?, CurrentAddress = ?, DateOfJoining = ?
+    WHERE StaffID = ?`;
+
+  const updateLoginQuery = `
+    UPDATE Login 
+    SET username = ?, password = ?
+    WHERE StaffID = ?`;
+
+  db.query(updateEmployeeQuery, [StaffName, PhoneNumber, DayOfBirth, DepartmentID, ProjectID, ReminderID, Email, employeeId], (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    db.query(updateStaffInfoQuery, [Gender, CCCD, Position, Birthplace, Nationality, Nation, Languages, CurrentAddress, DateOfJoining, employeeId], (err) => {
       if (err) {
-        console.error('Error executing query:', err);
         return res.status(500).send(err);
       }
-      res.json({ StaffID: employeeId, StaffName, PhoneNumber, DayOfBirth, DepartmentID, ProjectID, ReminderID });
+
+      db.query(updateLoginQuery, [username, password, employeeId], (err) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+
+        // Fetch updated data to return as response
+        const fetchUpdatedEmployeeQuery = `
+          SELECT 
+            Staff.StaffID AS EmployeeID,
+            Staff.StaffName AS EmployeeName,
+            StaffInfo.Gender,
+            StaffInfo.CCCD,
+            StaffInfo.Position,
+            StaffInfo.Birthplace,
+            StaffInfo.Nationality,
+            StaffInfo.Nation,
+            StaffInfo.Languages,
+            StaffInfo.CurrentAddress,
+            StaffInfo.DateOfJoining,
+            Staff.Email,
+            Staff.PhoneNumber,
+            Staff.DayOfBirth,
+            Department.Department,
+            Project.ProjectName,
+            Login.username,
+            Login.password
+          FROM Staff
+          LEFT JOIN StaffInfo ON Staff.StaffID = StaffInfo.StaffID
+          LEFT JOIN Department ON Staff.DepartmentID = Department.DepartmentID
+          LEFT JOIN Project ON Staff.ProjectID = Project.ProjectID
+          LEFT JOIN Login ON Staff.StaffID = Login.StaffID
+          WHERE Staff.StaffID = ?`;
+
+        db.query(fetchUpdatedEmployeeQuery, [employeeId], (err, results) => {
+          if (err) {
+            return res.status(500).send(err);
+          }
+          res.status(200).json(results[0]);
+        });
+      });
     });
   });
+});
+
 
 module.exports = router;
